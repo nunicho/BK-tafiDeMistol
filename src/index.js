8b989870fffd460d822b60feb9749887ab7376c8
Mauricio-Alonso <conta.alonso@gmail.com>
2022-11-11 16:22:53 -0300

    Armamos la base backend

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import morgan from "morgan"
+import cors from 'cors'
+import path from 'path'
+
+const  app = express();
+
+app.set('port', process.env.PORT ||  4000)
+
+app.listen(app.get('port'),()=>{
+    console.log('Estoy en el puerto '+ app.get('port'));
+})
+
+//middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, '../public')))
+console.log(path.join(__dirname, '../public'))
+
+//rutas: nombre de dominio + ---- 
+app.use('/apirestaurante', )
