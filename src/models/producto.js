3cbc58b150a0330931d02e8ce898dd3cfaf25929
Mauricio-Alonso <conta.alonso@gmail.com>
2022-11-13 14:09:14 -0300

    Creado el modelo producto y producto controllers

---
+++ src/models/producto.js
@@ -0,0 +1,46 @@
+import mongoose, {Schema} from "mongoose";
+
+
+//Schema es una clase de Moongose
+const productoSchema = new Schema({
+    nombreProducto:{
+        type: String,
+        required:true,
+        unique:true,
+        minLength:2,
+        maxLength:50, 
+    },
+    estado:{
+        type:String,
+        required:true,
+        minLength:2,
+        maxLength:500
+    },
+    precio:{
+        type:Number,
+        required:true,
+        min: 1,
+        max: 10000
+    },
+     detalle:{
+        type:String,
+        required:true,
+        minLength:2,
+        maxLength:10000 
+    },
+    categoria:{
+        type: String,
+        required:true,
+    },
+    imagen:{
+        type: String,
+        required:true
+    }
+      
+
+})
+
+// aqui realizamos el modelo
+const Producto = mongoose.model('producto', productoSchema);
+
+export default Producto;
