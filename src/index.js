c08b9ce9c8cc20dbc4850b61910e1c2771c17ba5
Mauricio-Alonso <conta.alonso@gmail.com>
2022-11-29 12:59:35 -0300

    Ajustes finales para deploy

--- src/index.js
+++ src/index.js
@@ -10,27 +10,21 @@ import './database'
 
 const app = express();
 
-
-
 app.set("port", process.env.PORT||4000)
-
 app.listen(app.get('port'),()=>{
     console.log('Estoy en el puerto '+ app.get('port'));
 })
 
-// middlewares: funciones que se ejecutan antes de las rutas
-// da información extra en la terminal 
+
 app.use(morgan('dev'));
-//permitir peticiones remotas
 app.use(cors());
-//middlewares para interpretar los objetos JSON 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-//cargar un archivo estático 
+
 app.use(express.static(path.join(__dirname, '../public')))
 console.log(path.join(__dirname, '../public'));    
 
-//rutas: nombre de dominio + ---- 
+
 app.use('/apirestaurante/pr', productosRouter )
 app.use('/apirestaurante/pe', pedidosRouter )
 app.use('/apirestaurante/us', usuariosRouter)
